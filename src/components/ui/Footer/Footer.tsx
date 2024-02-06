be3215bcae3cbb2a6fc683bf0651f80bb28495f3
Ivan Miyashiro <ivo.miyashiro1@gmail.com>
2024-02-05 23:40:43 -0300

    Add language icon

--- src/components/ui/Footer/Footer.tsx
+++ src/components/ui/Footer/Footer.tsx
@@ -1,13 +1,26 @@
 import { getSocialMedia } from '@/helpers/getSocialMedia'
+import { LanguageIcon } from '@/components/ui'
 
-const Footer = ({ i18n }: { i18n: FOOTER }) => {
+const Footer = ({ currentLocale, i18n }: {
+  currentLocale?: string
+  i18n: FOOTER
+}) => {
   const socialMedia = getSocialMedia()
+  const items = [
+    {
+      name: 'Lagunage',
+      href: `/${currentLocale === 'es' ? '' : 'es'}`,
+      target: '_self',
+      icon: LanguageIcon
+    },
+    ...socialMedia
+  ]
 
   return (
     <footer className="mt-24 flex flex-col items-center justify-center py-6">
       <p className="text-xs font-mono">{ i18n.TEXT }</p>
       <ul className="flex gap-4 mt-3">
-        { socialMedia.map(({ href, icon: Icon, target }, index) => (
+        { items.map(({ href, icon: Icon, target }, index) => (
           <li key={index}>
             <a href={href} target={target}>
               { <Icon
